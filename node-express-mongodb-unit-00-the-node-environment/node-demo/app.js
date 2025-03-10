3e8c100ea191d68bf8b232ddb22e6e0dfefcb5fe
steven <shotta3xd@gmail.com>
2025-03-10 14:32:00 -0400

    added .gitignore and unit 01

---
+++ node-express-mongodb-unit-00-the-node-environment/node-demo/app.js
@@ -0,0 +1,20 @@
+// testing node
+//console.log(" Hello Steven !");
+
+function Greetings(username) {
+  return "Hello " + username;
+}
+
+// remember that using conosle.log allows you to see the output in the terminal
+// if you run a function or method and you dont tell it to return anything, it will return undefined
+// also if you don't console.log the function, you won't see the output in the terminal
+
+console.log(Greetings("Steven H"));
+
+/*
+create 3 directories 
+name them what you want. 
+3 different dependencies for each directory
+
+
+*/
